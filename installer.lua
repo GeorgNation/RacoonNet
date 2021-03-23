7ce3926bb07fed649815c13e1d0c5c8ce66284dc
AlexCatze <samsung5552015.st@gmail.com>
2021-03-23 11:11:10 +0200

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,6 +1,6 @@
 local shell = require("shell")
 local fs = require("filesystem")
-local prefix = "https://raw.githubusercontent.com/AlexCatze/RacoonNet/alpha/main/"
+local prefix = "https://raw.githubusercontent.com/AlexCatze/RacoonNet/master/main/"
 local comp = require("computer")
 local files = {"/bin/chat.lua","/bin/chat_server.lua","/bin/ping.lua","/bin/rnconfig.lua","/bin/routconf.lua","/bin/router.lua","/bin/webserver.lua","/bin/wr.lua","/etc/config/sys.cfg","/etc/lang/ru.router.lang","/lib/opennet.lua","/lib/racoonnet.lua","/lib/rn_modem.lua","/lib/rn_stem.lua","/lib/rn_tunnel.lua",}
 
