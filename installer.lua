a934376865a8b4e69fd5dce0dee192b051288951
GeorgNation <55389952+GeorgNation@users.noreply.github.com>
2021-09-16 18:44:11 +0600

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,8 +1,8 @@
 local shell = require("shell")
 local fs = require("filesystem")
-local prefix = "https://raw.githubusercontent.com/AlexCatze/RacoonNet/master/main/"
+local prefix = "https://raw.githubusercontent.com/GeorgNation/RacoonNet/master/main/"
 local comp = require("computer")
-local files = {"/bin/chat.lua","/bin/chat_server.lua","/bin/ping.lua","/bin/rnconfig.lua","/bin/routconf.lua","/bin/router.lua","/bin/webserver.lua","/bin/wr.lua","/etc/config/sys.cfg","/etc/lang/ru.router.lang","/lib/opennet.lua","/lib/racoonnet.lua","/lib/rn_modem.lua","/lib/rn_stem.lua","/lib/rn_tunnel.lua",}
+local files = {"/bin/chat.lua","/bin/chat_server.lua","/bin/ping.lua","/bin/rnconfig.lua","/bin/routconf.lua","/bin/router.lua","/bin/webserver.lua","/bin/wr.lua","/etc/config/sys.cfg","/etc/lang/ru.router.lang","/lib/opennet.lua","/lib/racoonnet.lua","/lib/rn_modem.lua","/lib/rn_stem.lua","/lib/rn_tunnel.lua","/lib/dns.lua", "/bin/dnssrv.lua",}
 
 shell.execute("wget -f https://pastebin.com/raw/iKzRve2g /lib/forms.lua")
 shell.execute("wget -f https://pastebin.com/raw/C5aBuY5e /lib/rainbow.lua")
